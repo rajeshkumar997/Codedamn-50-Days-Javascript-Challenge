b804be61fe6940e34fb9d12d37f6167f411389bf
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-29 22:04:04 +0530

    Create index.js

---
+++ Day-25/index.js
@@ -0,0 +1,10 @@
+function spinWords(str) {
+    const words = str.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length >= 5) {
+            words[i] = words[i].split("").reverse().join("");
+        }
+    }
+    return words.join(" ");
+}
