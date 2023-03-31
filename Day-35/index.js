677a4b247c711be715435637d5bb20fa32540a90
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-31 21:42:24 +0530

    Create index.js

---
+++ Day-35/index.js
@@ -0,0 +1,10 @@
+function nthlargest(arr, highest) {
+	// write your code here
+  arr.sort((a,b) => b-a);
+	return arr[highest-1]
+}
+
+const arr = [43, 56, 23, 89, 88, 90, 99, 652];
+const highest = 4;
+
+console.log(nthlargest(arr, highest));
